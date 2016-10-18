87829dfaa5930142ec7431c7043851e464999dd8
shicy <shicy85@163.com>
2016-10-18 09:42:13 +0800

    框架事例修改，增加WEB服务器端口设置事例

--- samples/startup.js
+++ samples/startup.js
@@ -8,6 +8,7 @@ console.log(">> 开启服务（默认）");
 console.log(">> ========================================");
 console.log(">> 引用：var VRender = require('v-render');");
 console.log(">> 默认方式开启服务：VRender.create().initialize().run();");
+console.log(">> 打开浏览器输入http://localhost:8888，了解更多关于VRender的使用方法。");
 console.log(">>>");
 
 ///////////////////////////////////////////////////////////
