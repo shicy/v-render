8fec57527017baa21924931bcac96fa1fecc1d08
shicy <shicy85@163.com>
2016-11-07 17:01:25 +0800

    前端脚本、样式设计，一键压缩合并脚本和样式文件

---
+++ build/Gruntfile.js
@@ -0,0 +1,55 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+
+		uglify: {
+			pc: {
+				files: [{
+					src: ["VRender.front.js"],
+					dest: "VRender.front.min.js"
+				}]
+			},
+			mobile: {
+				files: [{
+					src: ["VRender.front.js"],
+					dest: "VRender.front.mobile.min.js"
+				}]
+			}
+		},
+
+		cssmin: {
+			pc: {
+				files: [{
+					src: ["VRender.front.css"],
+					dest: "VRender.front.min.css"
+				}]
+			},
+			mobile: {
+				files: [{
+					src: ["VRender.front.css"],
+					dest: "VRender.front.mobile.min.css"
+				}]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+
+	///////////////////////////////////////////////////////
+	grunt.registerTask("minify", function () {
+		grunt.file.setBase("../lib/static/");
+		grunt.task.run(["uglify", "cssmin"]);
+
+		var version = grunt.template.today("yymmddHHMM");
+		var files = ["PageView.js", "AppPageView.js", "WebPageView.js"];
+		for (var i = 0, l = files.length; i < l; i++) {
+			var filepath = "../ui/" + files[i];
+			var text = grunt.file.read(filepath).toString("utf-8");
+			text = text.replace(/\.min\.js\?v=.{10}/g, ".min.js?v=" + version);
+			text = text.replace(/\.min\.css\?v=.{10}/g, ".min.css?v=" + version);
+			grunt.file.write(filepath, text);
+		}
+	});
+
+};
