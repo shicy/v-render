d9a7a185e2dd3b1bf75a3b39d79e7b1ed990d0d1
shicy <shicy85@163.com>
2016-12-01 09:19:14 +0800

    框架设计说明文档修改，组件设计文档修改

--- docs/components/index.js
+++ docs/components/index.js
@@ -13,9 +13,23 @@ var SideMenuView = require("./SideMenuView");
 var Utils = VRender.Utils;
 
 var ComponentView = VRender.SinglePageView.extend(module, {
+	readyCode: "docs.components",
 
 	getPageTitle: function () {
 		return "VRender组件";
+	},
+
+	doInit: function () {
+		ComponentView.__super__.doInit.call(this);
+
+		var paths = this.options.pathname.substr(1).split("/");
+		this.moduleName = this.options.moduleName = paths[2];
+
+		var self = this;
+		this.moduleView = new ModuleView(this, this.options);
+		this.moduleView.ready(function () {
+			self.ready("docs.components");
+		});
 	},
 
 	renderBody: function (body) {
@@ -25,12 +39,10 @@ var ComponentView = VRender.SinglePageView.extend(module, {
 
 		var mainBody = VRender.$("<div id='main-body'></div>").appendTo(body);
 
-		var paths = this.options.pathname.substr(1).split("/");
-		new SideMenuView(this, {active: paths[2]}).render(mainBody);
+		new SideMenuView(this, {active: this.moduleName}).render(mainBody);
 
 		var container = VRender.$("<div class='container'></div>").appendTo(mainBody);
-		this.options.moduleName = paths[2];
-		new ModuleView(this, this.options).render(container);
+		this.moduleView.render(container);
 	},
 
 	getSinglePageContainer: function () {
@@ -38,4 +50,4 @@ var ComponentView = VRender.SinglePageView.extend(module, {
 	}
 });
 
-ComponentView.import("/theme/css/style.css");
+ComponentView.import(["/theme/css/prism.css", "/theme/css/style.css"]);
--- docs/components/module_button.js
+++ docs/components/module_button.js
@@ -8,9 +8,44 @@ var VRender = require("../../index");
 var ModuleBase = require("./module_base");
 
 
+var UIHGroup = VRender.UIHGroup;
 var UIButton = VRender.UIButton;
 
 
 var ModuleButton = ModuleBase.extend(module, {
-	className: "mod-button"
+	className: "mod-button",
+
+	getCompName: function () {
+		return "UIButton";
+	},
+
+	getSubName: function () {
+		return "按钮";
+	},
+
+	getDescription: function () {
+		return "按钮组件定义，在按钮元素<code>&lt;button&gt;</code>的基础上进行包装，预定义按钮的样式，扩展按钮功能支持下拉选择。";
+	},
+
+	renderView: function () {
+		ModuleButton.__super__.renderView.call(this);
+		this.showDemo1();
+	},
+
+	showDemo1: function () {
+		var section = this.appendSection("组件实例", "创建按钮使用不同的样式，可选择样式有：<code>.ui-btn-primary</code>," +
+			" <code>.ui-btn-success</code>, <code>.ui-btn-danger</code>, <code>.ui-btn-warn</code>, " +
+			"<code>.ui-btn-info</code>, <code>.ui-btn-link</code>, <code>.ui-btn-text</code>。设置按钮样式使用<code>style</code>属性。");
+
+		this.showMessage(section, "使用自定义样式", "当设置的样式<code>style</code>不在可选范围内时，将使用默认样式<code>.ui-btn-default</code>，" +
+			"自定义样式<code>style</code>作为类添加在按钮上。", "warn");
+
+		var demoView = new UIHGroup(this, {gap: 10});
+		demoView.append(new UIButton(this, {label: "Default"}));
+		demoView.append(new UIButton(this, {label: "Primary", style: "ui-btn-primary"}));
+
+		var source = "new UIButton(this, {label: \"积分微风姐\"}).render(target)";
+
+		this.showDemo(section, demoView, source);
+	}
 });
