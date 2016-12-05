51605c7186f5a21ae40b8fcbc5084e183d9ee261
shicy <shicy85@163.com>
2016-12-05 10:08:54 +0800

    按钮组件设计

--- docs/components/module_button.js
+++ docs/components/module_button.js
@@ -4,13 +4,13 @@
 // Create on 2016-11-28
 // ========================================================
 
-var VRender = require("../../index");
+var VRender = require(__vrender__);
 var ModuleBase = require("./module_base");
 
 
+var UIGroup = VRender.UIGroup;
 var UIHGroup = VRender.UIHGroup;
 var UIButton = VRender.UIButton;
-
 
 var ModuleButton = ModuleBase.extend(module, {
 	className: "mod-button",
@@ -30,6 +30,7 @@ var ModuleButton = ModuleBase.extend(module, {
 	renderView: function () {
 		ModuleButton.__super__.renderView.call(this);
 		this.showDemo1();
+		this.showDemo2();
 	},
 
 	showDemo1: function () {
@@ -43,9 +44,59 @@ var ModuleButton = ModuleBase.extend(module, {
 		var demoView = new UIHGroup(this, {gap: 10});
 		demoView.append(new UIButton(this, {label: "Default"}));
 		demoView.append(new UIButton(this, {label: "Primary", style: "ui-btn-primary"}));
+		demoView.append(new UIButton(this, {label: "Success", style: "ui-btn-success"}));
+		demoView.append(new UIButton(this, {label: "Danger", style: "ui-btn-danger"}));
+		demoView.append(new UIButton(this, {label: "Warn", style: "ui-btn-warn"}));
+		demoView.append(new UIButton(this, {label: "Info", style: "ui-btn-info"}));
+		demoView.append(new UIButton(this, {label: "Link", style: "ui-btn-link"}));
+		demoView.append(new UIButton(this, {label: "Text", style: "ui-btn-text"}));
 
-		var source = "new UIButton(this, {label: \"积分微风姐\"}).render(target)";
+		var source = [];
+		source.push("new UIButton(this, {label: \"Default\"});");
+		source.push("new UIButton(this, {label: \"Primary\", style: \"ui-btn-primary\"})");
+		source.push("new UIButton(this, {label: \"Success\", style: \"ui-btn-success\"})");
+		source.push("new UIButton(this, {label: \"Danger\", style: \"ui-btn-danger\"})");
+		source.push("new UIButton(this, {label: \"Warn\", style: \"ui-btn-warn\"})");
+		source.push("new UIButton(this, {label: \"Info\", style: \"ui-btn-info\"})");
+		source.push("new UIButton(this, {label: \"Link\", style: \"ui-btn-link\"})");
+		source.push("new UIButton(this, {label: \"Text\", style: \"ui-btn-text\"})");
 
-		this.showDemo(section, demoView, source);
+		this.showDemo(section, demoView, source.join("\n"));
+	},
+
+	showDemo2: function () {
+		var section = this.appendSection("按钮尺寸", "需要按钮的不同尺寸大小，设置<code>size</code>属性，从小到大依次是<code>tiny</code>, " +
+			"<code>small</code>, <code>normal</code>, <code>big</code>, <code>large</code>，默认是<code>normal</code>。");
+
+		var demoView = new UIGroup(this, {gap: 10});
+		var group = demoView.addChild(new UIHGroup(this, {gap: 10}));
+		group.append(new UIButton(this, {label: "tiny button", size: "tiny"}));
+		group.append(new UIButton(this, {label: "tiny button", size: "tiny", style: "ui-btn-primary"}));
+		var group = demoView.addChild(new UIHGroup(this, {gap: 10}));
+		group.append(new UIButton(this, {label: "small button", size: "small"}));
+		group.append(new UIButton(this, {label: "small button", size: "small", style: "ui-btn-primary"}));
+		var group = demoView.addChild(new UIHGroup(this, {gap: 10}));
+		group.append(new UIButton(this, {label: "normal button", size: "normal"}));
+		group.append(new UIButton(this, {label: "normal button", size: "normal", style: "ui-btn-primary"}));
+		var group = demoView.addChild(new UIHGroup(this, {gap: 10}));
+		group.append(new UIButton(this, {label: "big button", size: "big"}));
+		group.append(new UIButton(this, {label: "big button", size: "big", style: "ui-btn-primary"}));
+		var group = demoView.addChild(new UIHGroup(this, {gap: 10}));
+		group.append(new UIButton(this, {label: "large button", size: "large"}));
+		group.append(new UIButton(this, {label: "large button", size: "large", style: "ui-btn-primary"}));
+
+		var source = [];
+		source.push("new UIButton(this, {label: \"tiny button\", size: \"tiny\"});");
+		source.push("new UIButton(this, {label: \"tiny button\", size: \"tiny\", style: \"ui-btn-primary\"});");
+		source.push("new UIButton(this, {label: \"small button\", size: \"small\"});");
+		source.push("new UIButton(this, {label: \"small button\", size: \"small\", style: \"ui-btn-primary\"});");
+		source.push("new UIButton(this, {label: \"normal button\", size: \"normal\"});");
+		source.push("new UIButton(this, {label: \"normal button\", size: \"normal\", style: \"ui-btn-primary\"});");
+		source.push("new UIButton(this, {label: \"big button\", size: \"big\"});");
+		source.push("new UIButton(this, {label: \"big button\", size: \"big\", style: \"ui-btn-primary\"});");
+		source.push("new UIButton(this, {label: \"large button\", size: \"large\"});");
+		source.push("new UIButton(this, {label: \"large button\", size: \"large\", style: \"ui-btn-primary\"});");
+
+		this.showDemo(section, demoView, source.join("\n"));
 	}
 });
