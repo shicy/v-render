b3bd3b85fe0b693e91589e239ce0851990b6149a
cyshi <cyshi@zhcpa.cn>
2017-12-15 14:09:50 +0800

    组件 radiobox 重构

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -9,18 +9,20 @@ module.exports = function (grunt) {
 	commonScripts.push("js/render/_base.js");
 	commonScripts.push("js/render/group.js");
 	commonScripts.push("js/render/button.js");
+	commonScripts.push("js/render/checkbox.js");
+	commonScripts.push("js/render/radiobox.js");
+	commonScripts.push("js/render/textview.js");
 	commonScripts.push("js/render/dropdownlist.js");
 	commonScripts.push("js/render/combobox.js");
-	commonScripts.push("js/render/textview.js");
-	commonScripts.push("js/render/checkbox.js");
-	
+
 	commonScripts.push("js/ui/_base.js");
 	commonScripts.push("js/ui/group.js");
 	commonScripts.push("js/ui/button.js");
+	commonScripts.push("js/ui/checkbox.js");
+	commonScripts.push("js/ui/radiobox.js");
+	commonScripts.push("js/ui/textview.js");
 	commonScripts.push("js/ui/dropdownlist.js");
 	commonScripts.push("js/ui/combobox.js");
-	commonScripts.push("js/ui/textview.js");
-	commonScripts.push("js/ui/checkbox.js");
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
