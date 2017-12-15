fcbd61973d220a37a3c17b0bfb2a4ec6501d5d77
cyshi <cyshi@zhcpa.cn>
2017-12-15 11:58:54 +0800

    组件 textview 重构

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -11,11 +11,13 @@ module.exports = function (grunt) {
 	commonScripts.push("js/render/group.js");
 	commonScripts.push("js/render/combobox.js");
 	commonScripts.push("js/render/button.js");
+	commonScripts.push("js/render/textview.js");
 	commonScripts.push("js/ui/_base.js");
 	commonScripts.push("js/ui/dropdownlist.js");
 	commonScripts.push("js/ui/group.js");
 	commonScripts.push("js/ui/combobox.js");
 	commonScripts.push("js/ui/button.js");
+	commonScripts.push("js/ui/textview.js");
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON("package.json"),
