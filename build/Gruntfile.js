c91bf3b91003e0c1566ed5659ddd8579d29ff7e3
shicy <shicy85@163.com>
2018-06-20 18:54:49 +0800

    添加组件 fileupload

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -26,6 +26,8 @@ module.exports = function (grunt) {
 	commonScripts.push("js/ui/dateinput.js");
 	commonScripts.push("js/render/daterange.js");
 	commonScripts.push("js/ui/daterange.js");
+	commonScripts.push("js/render/fileupload.js");
+	commonScripts.push("js/ui/fileupload.js");
 	commonScripts.push("js/render/formview.js");
 	commonScripts.push("js/ui/formview.js");
 	commonScripts.push("js/render/popupmenu.js");
