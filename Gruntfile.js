d2c04bc117f1763ec2dd78a15c3810f3a06b73d3
shicy <shicy85@163.com>
2019-05-22 15:48:33 +0800

    前端构建方法修改，去掉前端样式构建，取消脚本构建版本，框架脚本合并打包

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,41 +26,14 @@ module.exports = function (grunt) {
 					dest: "VRender.front.mobile.min.js"
 				}]
 			}
-		},
-
-		cssmin: {
-			pc: {
-				files: [{
-					src: ["VRender.front.css"],
-					dest: "VRender.front.min.css"
-				}]
-			},
-			mobile: {
-				files: [{
-					src: ["VRender.front.mobile.css"],
-					dest: "VRender.front.mobile.min.css"
-				}]
-			}
 		}
 	});
 
 	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-cssmin");
 
 	///////////////////////////////////////////////////////
 	grunt.registerTask("minify", function () {
 		grunt.file.setBase("./lib/static/");
-		grunt.task.run(["uglify", "cssmin"]);
-
-		var version = grunt.template.today("yymmdd");
-		var files = ["PageView.js", "AppPageView.js", "WebPageView.js"];
-		for (var i = 0, l = files.length; i < l; i++) {
-			var filepath = "../ui/" + files[i];
-			var text = grunt.file.read(filepath).toString("utf-8");
-			text = text.replace(/\.min\.js\?v=.{6}/g, ".min.js?v=" + version);
-			text = text.replace(/\.min\.css\?v=.{6}/g, ".min.css?v=" + version);
-			grunt.file.write(filepath, text);
-		}
+		grunt.task.run(["uglify"]);
 	});
-
 };
